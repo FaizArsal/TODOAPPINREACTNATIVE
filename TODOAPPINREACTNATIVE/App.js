2794c81fcba3fba03e8c8f07163d06c1a7967a61
Faiz E Arsal <faizarsal066@gmail.com>
2022-12-08 20:54:50 +0500

    first commit

---
+++ TODOAPPINREACTNATIVE/App.js
@@ -0,0 +1,49 @@
+import { SafeAreaView, ScrollView } from "react-native";
+
+// components
+import Login1 from "./screen/Login1";
+import Login2 from "./screen/Login2";
+import Login3 from "./screen/Login3";
+import Login4 from "./screen/Login4";
+import Login5 from "./screen/Login5";
+import Todo from "./screen/Todo";
+
+
+const App = () => {
+  return (
+    <>
+      {/* <Login1 /> */}
+      {/* < Login2 /> */}
+      {/* < Login3 /> */}
+      {/* <Login4 /> */}
+      {/* <Login5 /> */}
+      <Todo />
+    </>
+  )
+};
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ TODOAPPINREACTNATIVE/screen/Login2.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+import { View, Text, StyleSheet, SafeAreaView, ScrollView, TextInput, Button, Alert, StatusBar } from 'react-native'
+
+const Login2 = () => {
+    let [password, setPassword] = useState("");
+    return (
+        <SafeAreaView>
+            <ScrollView contentContainerStyle={{ width: "100%", height: "100%", justifyContent: "center", alignItems: "center", backgroundColor: "#8d99ae" }}>
+                <StatusBar backgroundColor={"#8d99ae"} />
+
+                
+
+                <View style={{ width: "90%", padding: 10 }}>
+                    <View>
+                        <Text style={{ fontSize: 30, fontWeight: "bold", color: "#14213d", paddingVertical: 20 }}>Login</Text>
+                    </View>
+                    <View>
+                        <View>
+                            <TextInput style={{ borderWidth: 2, borderStyle: "solid", borderColor: "#ccc", borderRadius: 5, padding: 10, fontSize: 17, color: "#14213d", marginVertical: 5 }} placeholder="Enter email" />
+
+                            <TextInput style={{ borderWidth: 2, borderStyle: "solid", borderColor: "#ccc", borderRadius: 5, padding: 10, fontSize: 17, color: "#14213d", marginVertical: 5 }}
+                                value={"*".repeat(password.length)}
+                                placeholder="Enter password" onChangeText={(e) => setPassword(e)} />
+
+                            <View style={{ marginVertical: 20 }}>
+                                <Button onPress={() => { Alert.alert("Alert", "Successfully Login", [{ text: "OK" }]) }} color={"#14213d"} title="Login" />
+                            </View>
+                        </View>
+                        <View>
+                            <Text style={{ fontSize: 16, }}>
+                                Do you not yet have an account? {" "}
+                                <Text style={{ color: "#14213d", fontWeight: "bold" }} onPress={() => { Alert.alert("Alert", "Sorry This page not working currently 😌", [{ text: "OK" }]) }}>Create Account</Text>
+                            </Text>
+                        </View>
+                    </View>
+                </View>
+
+            </ScrollView>
+        </SafeAreaView>
+    )
+}
+
+const Style = StyleSheet.create({
+
+});
+
+export default Login2;
